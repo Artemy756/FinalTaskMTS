fb685e204db126a18678663ac0020dc0154124d7
Artemy756 <pavlenko.artemii@phystech.edu>
2023-11-28 20:21:10 +0300

    create structure

--- src/main/java/org/roombooking/entity/User.java
+++ src/main/java/org/roombooking/entity/User.java
@@ -1,4 +1,17 @@
 package org.roombooking.entity;
 
+import org.roombooking.entity.id.UserId;
+
 public class User {
+  private UserId userId;
+  private String name;
+  private String phoneNumber;
+  private String email;
+
+  public User(UserId userId, String name, String phoneNumber, String email) {
+    this.userId = userId;
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
 }
